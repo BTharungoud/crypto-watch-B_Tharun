ebd5ef65dbe6a967ce104081efd6829b41efb6ac
BTharungoud <122255721+BTharungoud@users.noreply.github.com>
2023-02-28 13:08:52 +0530

    step-1-completed

---
+++ src/Pages/LoginPage.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { useHistory, Link } from 'react-router-dom'
+import './auth.css'
+const LoginPage = (props) => {
+  console.log(props)
+  const [credentials, setCredentials] = useState({ email: '', password: ''})
+ 
+  const history = useHistory()
+  const obj2={ email: '', password: ''}
+  const handleLogin = (e) => {
+
+    e.preventDefault();
+   // console.log(history);
+    
+    // get the user data from the localStorage if matches take the user to Home page
+
+  }
+  const handleChange = (e) => {
+    e.preventDefault();
+    setCredentials({...credentials ,[e.target.name]: e.target.value })
+  }
+
+  return (
+    <div className='container'>
+      <form onSubmit={handleLogin}>
+        <h1>Login</h1>
+        <div className="input email-input">
+          <input type="email" placeholder='Enter you email..' required value={credentials.email} name="email" onChange={handleChange} />
+        </div>
+        <div className="input password-input">
+          <input type="password" placeholder='Enter you password' required value={credentials.password} name="password" onChange={handleChange} />
+        </div>
+        <button type='submit'>Login</button>
+        <div className="account">
+          <span>Don't have an account?</span>
+          <Link to="/">Register</Link>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default LoginPage
